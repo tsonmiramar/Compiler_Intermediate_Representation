cff64ef17779afae58c48ac257db0e60ddf8c9b4
Son T Do <sdo@ieng6-201.ucsd.edu>
2017-03-12 18:14:07 -0700

    testing file

---
+++ glc.glsl
@@ -0,0 +1,22 @@
+void main(int a, int b){
+	int c = 3;
+	bool d = true;
+	{
+		int e;
+		{
+			int f;
+			f = 4;
+		}
+		e = 4;
+	}
+
+	while (true){	
+	
+		int g;
+	}
+	
+}
+
+void foo() {
+	int b;
+}
